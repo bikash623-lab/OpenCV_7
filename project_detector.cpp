79bd860bc1ed16f57ad2d665b739bd0d8213fa8f
Bikash Malla <bikashmalla80@gmail.com>
2026-07-24 15:34:34 -0500

    Update all changes

--- project_detector.cpp
+++ project_detector.cpp
@@ -2,3 +2,21 @@
 #include <opencv2/opencv.hpp>
 #include <chrono>
 
+int main(){
+    // Open Camera
+    cv:: VideoCapture cap(0);
+    if(!cap.isOpened()){
+        std::cout<<"Error:couldn't open camera.";
+        return -1;
+    }
+
+    cap.set(cv::CAP_PROP_FRAME_WIDTH, 1280)
+    cap.set(cv::CAP_PROP_FRAME_HEIGHT, 720);
+
+    std::cout<<"Width:"<<cap.get(cv::CAP_PROP_FRAME_WIDTH) <<"\n";
+    std::cout<<"Heigth:"<<cap.get(cv::CAP_PROP_FRAME_HEIGHT)<<"\n";
+    std::cout<<"FPS:"<<cap.get(cv::CAP_PROP_FPS)<<"\n";
+
+
+    
+}
